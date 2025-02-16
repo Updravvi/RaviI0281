a5688eae1682acf1f694b057381d9d779c496b88
Ravi Upadhyay <UpadhyayR@incadeadev.com>
2025-02-16 14:58:45 +0530

    EDFSF

---
+++ Assignments/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Ravi!");
+
+            Console.WriteLine("Welcome");
+
+
+        }
+    }
+}
