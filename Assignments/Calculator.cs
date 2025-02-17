816fd75cc4b4ae8f493051bcb2c1bebf4def0869
Ravi Upadhyay <UpadhyayR@incadeadev.com>
2025-02-17 15:06:17 +0530

    Class Calculator

---
+++ Assignments/Calculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    public class Calculator
+    {
+
+        int a, b, c;
+        char ch;
+
+        public void CalculatorOperation()
+        
+        {
+            Console.WriteLine("Calculator");
+            Console.Write("Enter 1st Number : ");
+
+            //string s = Console.ReadLine();
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter 2nd Number : ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Select operation (+,-,*,/) ");
+            ch = char.Parse(Console.ReadLine());
+
+            switch (ch)
+            {
+                case '+':
+
+                    c = Add(a, b);
+                    Console.WriteLine("Addition : " + c);
+                    break;
+
+                case '-':
+                    c = Sub(a, b);
+                    Console.WriteLine("Substraction  : " + c);
+                    break;
+
+                case '*':
+                    c = Mul(a, b);
+                    Console.WriteLine("Multiplicatio: " + c);
+                    break;
+
+                case '/':
+                    c = Div(a, b);
+                    Console.WriteLine("Division : " + c);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+            Console.Write("Enter to exit ");
+            Console.ReadLine(); 
+        }
+
+        //Add
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        //Sub
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        //Mul
+        public int Mul(int x, int y)
+        {
+            return x * y;
+        }
+
+        //Div
+        public int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+    }
+}
--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -8,73 +8,26 @@ namespace Assignments
         {
             Console.WriteLine("Hello, Ravi!");
 
-            Console.WriteLine("Multiplication Table Using a For Loop");
-            Console.Write(" Enter the Number : ");
+            //Console.WriteLine("Multiplication Table Using a For Loop");
+            //Console.Write(" Enter the Number : ");
 
-            //string s = Console.ReadLine();
-            int x = int.Parse(Console.ReadLine());
-            Console.Write(" Enter Table of :" + x);
+            ////string s = Console.ReadLine();
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write(" Enter Table of :" + x);
+            //Table(x);
 
-            Table(x);
+            //void Table(int x)
+            //{
+            //    for (int i = 1; i <= 10; i++)
+            //    {
+            //        Console.WriteLine(x + " *" + i + " = " + x * i);
+            //    }
 
-            Console.WriteLine("Calculator");
-            Console.Write("Enter 1st Number : ");
+            //}
+            Calculator ca = new Calculator();
 
-            //string s = Console.ReadLine();
-            int a = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter 1st Number : ");
-            int b = int.Parse(Console.ReadLine());
-            int c;
-
-            c = Add(a, b);
-            Console.WriteLine("Addition : " + c);
-
-            c = Sub(a, b);
-            Console.WriteLine("Substraction  : " + c);
-
-            c = Mul(a, b);
-            Console.WriteLine("Multiplicatio: " + c);
-
-            c = Div(a, b);
-            Console.WriteLine("Division : " + c);
-
-            //Add
-            int Add(int x, int y)
-            {
-                return x + y;
-            }
-
-            //Sub
-            int Sub(int x, int y)
-            {
-                return x - y;
-            }
-
-            //Mul
-            int Mul(int x, int y)
-            {
-                return x * y;
-            }
-
-            //Div
-            int Div(int x, int y)
-            {
-                return x / y;
-            }
-
-            void Table(int x)
-            {
-                for (int i = 1; i <= 10; i++)
-                {
-                    Console.WriteLine(x + " *" + i + " = " + x * i);
-                }
-
-            }
-
-
-
-
+            ca.CalculatorOperation();
+            Console.ReadLine();
         }
     }
 }
